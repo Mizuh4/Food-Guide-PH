61f01839945dcd66eab931d7cf32e13f3943f295
Gabe <gavila_02@mtc.edu.ph>
2024-07-08 16:52:25 +0800

    edited Django App Recipes

--- FGPH/recipes/admin.py
+++ FGPH/recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Author, Recipe
+
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("recipes",)
+    
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+
+admin.site.register(Author)
+admin.site.register(Recipe)
--- FGPH/recipes/models.py
+++ FGPH/recipes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
+
+class Recipe(models.Model):
+    REGIONS = {
+        "RegionI" : "Ilocos Region",
+        "RegionII" : "Cagayan Valley",
+        "RegionIII" : "Central Luzon",
+        "RegionIV‑A" : "CALABARZON",
+        "MIMAROPA" : "MIMAROPA Region",
+        "RegionV" : "Bicol Region",
+        "CAR" : "Cordillera Administrative Region",
+        "NCR" : "National Capital Region" 
+    }
+
+    name = models.CharField(max_length=171)
+    description = models.CharField(max_length=750)
+    region = models.CharField(max_length=10, choices=REGIONS)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="recipes")
+    ingredients = models.JSONField()
+
+    def __str__(self):
+        return f"{self.name}, desc: {self.description}, region: {self.region}, author: {self.author}, ingredients: {self.ingredients}"
