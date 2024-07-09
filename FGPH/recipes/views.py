3e9982dfb1c61fb78645b22c7ae1dc69b96ebf86
Gabe <gavila_02@mtc.edu.ph>
2024-07-09 13:38:15 +0800

    worked on html layouts

--- FGPH/recipes/views.py
+++ FGPH/recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,3 +6,16 @@ def index(request):
     return render(request, "recipes/index.html", {
         
     })
+
+def login_view(request):
+    return render(request, "recipes/login.html", {
+        
+    })
+
+def logout_view(request):
+    return render(request, "recipes/logout.html", {
+        
+    })
+
+def signup_view(request):
+    return HttpResponse("Signup View")
